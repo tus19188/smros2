2bfd00adf4765d35c125fa5a809955f60d9819ed
tus19188 <143554274+tus19188@users.noreply.github.com>
2023-10-01 22:08:25 -0400

    Update talker.py

--- demo_nodes_py/demo_nodes_py/topics/talker.py
+++ demo_nodes_py/demo_nodes_py/topics/talker.py
@@ -17,20 +17,22 @@ from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
 from std_msgs.msg import String
+import inflect  # Import the 'inflect' library
 
 
 class Talker(Node):
 
     def __init__(self):
-        super().__init__('talker')
+        super().__init__('Mayo')  # Replace 'your_last_name' with your actual last name
         self.i = 0
         self.pub = self.create_publisher(String, 'chatter', 10)
         timer_period = 1.0
         self.tmr = self.create_timer(timer_period, self.timer_callback)
-
+        self.p = inflect.engine()  # Create an instance of the inflect engine
+    
     def timer_callback(self):
         msg = String()
-        msg.data = 'Hello World: {0}'.format(self.i)
+        msg.data = 'Hello World: {0}'.format(self.p.number_to_words(self.i))  # Convert number to words
         self.i += 1
         self.get_logger().info('Publishing: "{0}"'.format(msg.data))
         self.pub.publish(msg)
