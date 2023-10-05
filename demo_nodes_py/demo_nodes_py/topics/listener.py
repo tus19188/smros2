00a0d32f6751539c8e2e7eb737c81825a341e9af
tus19188 <143554274+tus19188@users.noreply.github.com>
2023-10-05 18:47:59 -0400

    Update listener.py

--- demo_nodes_py/demo_nodes_py/topics/listener.py
+++ demo_nodes_py/demo_nodes_py/topics/listener.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+python
+Copy code
 import rclpy
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
